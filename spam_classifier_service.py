bd34df22104efcdffaef6a536a8d13684720c7a6
Charis Tourountzis <charistourou@gmail.com>
2023-12-08 20:24:01 +0200

    Removed some lines of code

--- spam_classifier_service.py
+++ spam_classifier_service.py
@@ -5,15 +5,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from pathlib import Path
-import joblib
 
 class SpamClassifierService:
-    def __init__(self,model_file="spam_model.joblib"):
+    def __init__(self,):
         self.cv = CountVectorizer()
         self.clf = MultinomialNB()
         self.score = None
-        self.model_file = model_file
-        self.load_model()
+        
 
     def train_model(self, data_path):
         data = pd.read_csv(data_path)
@@ -23,11 +21,6 @@ class SpamClassifierService:
         X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
         self.clf.fit(X_train, y_train)
         self.score = self.clf.score(X_train, y_train)
-        joblib.dump(self.clf, self.model_file)
-
-    def load_model(self):
-        if Path(self.model_file).is_file():
-            self.clf = joblib.load(self.model_file)
 
     def get_score(self):
         return self.score
